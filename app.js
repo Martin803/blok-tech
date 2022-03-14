be7aad9a66bd1498e3504327e0280def77b10d6d
Martin803 <82580596+Martin803@users.noreply.github.com>
2022-03-14 21:26:02 +0100

    huiswerk week 5

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ let db = null;
 console.log(process.env.TESTVAR)
 
 app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
 
@@ -16,6 +18,42 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.get('/', async (req, res) => {
+
+  const formdata = await db.collection('formdata').find({}).toArray();
+
+  res.render('profile')
+})
+
+app.post('/profile', async (req, res) => {
+    // ADD MOVIE 
+    let form = {
+        vak: req.body.vak, 
+        nat: req.body.nat, 
+        hoelang: req.body.hoelang
+    };
+    // TODO
+    await db.collection('formdata').insertOne(form);
+
+    const formdata = await db.collection('formdata').findOne(query);
+    
+    res.render('profile')
+});
+
+app.get('/profile', async (req, res) => {
+    const query = {_id: ObjectId("622f2a2312d4e2c45e47a19a")};
+    const profiles = await db.collection('formdata').findOne(query);
+
+    const title = "formdata";
+    res.render('profile', {title, formdata});
+})
+
+app.get('/edit', (req, res) => {
+    const title = "Profile Editor";
+    res.render('edit', {title, interests});
+})
+
+// Aan de hand van de totorial van Sonja
 
 app.get('/profile/:name', function (req, res) {
   var data = {age: 29, job: 'ufopiloot'}
@@ -46,6 +84,8 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+// connect to database
+
 async function connectDB() {
     const uri = process.env.DB_URI;
     const client = new MongoClient(uri, {
@@ -61,4 +101,3 @@ async function connectDB() {
 }
 
     connectDB().then(() => console.log("We have a connection to Mongo!"));
-
---
+++ readme.md
@@ -0,0 +1,33 @@
+# blok-tech
+
+Voor blok-tech ben ik aan de gang gegaan met een matching app die mensen met een klus matcht aan klussers die deze klus kunnen doen voor die persoon. De feature die ik ga uitwerken is een filter functie om de werklui te sorteren.
+
+# Installatie
+
+Wil je werken aan dit project?
+volg deze stappen
+
+1. clone deze repo
+
+```
+git clone https://github.com/Martin803/blok-tech.git
+```
+
+2. ga naar de map en typ deze command in de terminal
+```
+npm install
+```
+
+3. om het project te gebruiken
+```
+npm start
+```
+
+# Code of Conconduct
+
+de enige regels die je moet volgen om mee te werken aan dit project: Hou het netjes en maak geen ruzie
+dat is het voor de rest mag je helemaal los gaan
+
+# License
+
+Voor dit project is een APACHE 2.0 license gebruikt meer info hier:https://github.com/Martin803/blok-tech/blob/main/LICENSE
