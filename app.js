c5a0275863c2868c857dde6ee6b98560d4305e3a
Martin803 <82580596+Martin803@users.noreply.github.com>
2022-03-18 13:50:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000;
 const dotenv = require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const { ObjectId } = require('mongodb');
